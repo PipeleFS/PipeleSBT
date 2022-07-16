0fe1cce177050d1b44e9648952793d1963c8a4d9
darahask <darahask12@gmail.com>
2022-07-17 00:29:01 +0530

    pipele sbt v1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,18 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const pSBT = await hre.ethers.getContractFactory("PipeleSBT");
+  const psbt = await pSBT.deploy();
+  await psbt.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Pipele deployed to: ", psbt.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
